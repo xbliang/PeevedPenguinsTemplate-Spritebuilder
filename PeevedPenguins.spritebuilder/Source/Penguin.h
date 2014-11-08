99fcfe3dc84cd0d1b8e21de04c649e37a1883042
bobliang <xinyuliang42@gmail.com>
2014-11-08 14:53:48 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Penguin.h
@@ -0,0 +1,13 @@
+//
+//  Penguin.h
+//  PeevedPenguins
+//
+//  Created by Bob Liang on 11/8/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Penguin : CCSprite
+
+@end
---
+++ PeevedPenguins.spritebuilder/Source/Seal.h
@@ -0,0 +1,13 @@
+//
+//  Seal.h
+//  PeevedPenguins
+//
+//  Created by Bob Liang on 11/8/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Seal : CCSprite
+
+@end
