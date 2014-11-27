741ff216804f5aaab60fa5ff7342897875f2b0ba
bobliang <xinyuliang42@gmail.com>
2014-11-26 21:12:30 -0600

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -20,6 +20,9 @@
     self.userInteractionEnabled = TRUE;
     CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
     [_levelNode addChild:level];
+    // visualize physics bodies & joints
+    _physicsNode.debugDraw = TRUE;
+    
 }
 
 // called on every touch in this scene
